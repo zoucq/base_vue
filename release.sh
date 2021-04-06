cf834eddb99ca1842fa0844f9cc8d630b4c91811
zcq <zcq@91xiangju.com>
2021-04-06 14:08:21 +0800

    Update: release/0.1.14

--- release.sh
+++ release.sh
@@ -2,8 +2,4 @@
 rm -r -f ./qm-publish-project
 git clone https://gitlab.malmam.com/qm/qm-publish-project.git
 sh ./qm-publish-project/build.sh
-# 拷贝项目文件到外层目录，解决运行环境问题
-#cp ./qm-publish-project/build.js ./build.js
-#node ./build.js
-#node ./qm-publish-project/build.js
 exit 1;
