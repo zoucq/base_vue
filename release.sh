7ba53037650529039a5bb3e6e6c1e7a4d0c9ab13
zcq <zcq@91xiangju.com>
2021-04-06 16:32:07 +0800

    Update: release/0.1.25

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 # 本地存在不会重新clone，无法更新发布模板项目，这里直接删除目录
 #rm -r -f ./qm-publish-project
-#git clone https://gitlab.malmam.com/qm/qm-publish-project.git
+git clone https://gitlab.malmam.com/qm/qm-publish-project.git
 sh ./qm-publish-project/build.sh
 exit 1;
